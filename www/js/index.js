d96acb1fbb6a59626e3b70a16c2fe3cd392ca31d
sumitjain- <sumeetsuniljain@gmail.com>
2013-09-26 16:47:40 +0530

    Pushplugin setup 1

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,18 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     
+    tokenHandler:function(msg) {
+        alert("Token Handler " + msg);
+        regID = msg ;
+    },
+    errorHandler:function(error) {
+        console.log("Error Handler " + error);
+        alert(error);
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Success! Result = '+result)
+    },
     checkConnection: function(){
         var networkState = navigator.connection.type;
 
@@ -60,10 +72,72 @@ var app = {
         var parentElement = document.getElementById(id);
         navigator.notification.alert("UUID: "+device.uuid+" , Platform: "+device.platform, app.alertCallback);
 //        alert("generic alert working");
-        app.checkConnection();
         feed_init();
+        app.checkConnection();
+        var pushNotification = window.plugins.pushNotification;
+        // TODO: Enter your own GCM Sender ID in the register call for Android
+        if (device.platform == 'android' || device.platform == 'Android') {
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"51975193249","ecb":"app.onNotificationGCM"});
+        }
+        else {
+            pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"false","alert":"true","ecb":"app.onNotificationAPN"});
+//            alert("here");
+        }
         console.log('Received Event: ' + id);
+    },
+    
+    // iOS
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        console.log("Received a notification! " + event.alert);
+        console.log("event sound " + event.sound);
+        console.log("event badge " + event.badge);
+        console.log("event " + event);
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            console.log("Set badge on " + pushNotification);
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+    },
+    // Android
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    //document.getElementById('txtreg').value = e.regid;
+                    regID = e.regid;
+                    //alert('registration id = '+e.regid);
+                }
+            break;
+
+            case 'message':
+              // this is the actual push notification. its format depends on the data model
+              // of the intermediary push server which must also be reflected in GCMIntentService.java
+              alert(e.message);
+            break;
+
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
 };
+
+
+
 
 
